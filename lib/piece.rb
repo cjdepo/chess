fbe173201ca874075bd7ceb5c3faf4fa453a51d0
cjdepo <cjdepowell@protonmail.com>
2021-12-30 07:47:21 -0500

    possible_moves knight class

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,15 +1,38 @@
 # lib/piece
 
-# all piece types inherit from piece
 
-class Piece
+class Knight
+  attr_reader :moves
 
   def initialize(position)
-    @positon = position
+    @position = position
+    @moves = [[1,2],[2,1],[-1,2],[2,-1],[1,-2],[-2,-1],[-2,1],[-1,-2]]
   end
 
-  def possible_moves(position)
-    # returns possible moves from a certain position
+  def possible_moves(position_arr)
+    possible_positions = @moves.map{|move| move.map.with_index{|v, i| v + position_arr[i]}}
+    possible_positions.map! do |pos|
+      if pos.any?{ |v| v > 8 || v < 1 }
+        nil
+      else
+        pos
+      end
+    end.compact!
+    possible_positions
   end
-    
+
+
+
+    # convert position to [x, y] notation
+    # add each moves to xy
+    # filter out side of board values
+    # return positions
+
+
 end
+
+knight = Knight.new('e3')
+knight.possible_moves([7,5])
+
+
+  
