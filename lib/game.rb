19ad897ee7bb9b0b27371360de8021846a33f4cb
cjdepo <cjdepowell@protonmail.com>
2021-12-30 07:49:34 -0500

    position_to_arr and arr_to_position

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,8 @@
 # /lib/game
 
-class Game 
+class Game
   attr_reader :board
-  
+
   def initialize
     @rows = [1, 2, 3, 4, 5, 6, 7, 8]
     @columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
@@ -25,6 +25,36 @@ class Game
       row << board[position]
     end
     rows
+  end
+
+  def position_to_arr(position)
+    arr = []
+    @rows.each do |row|
+        if position[0] == row.to_s
+            arr[0] = row.to_i
+        end
+    end
+    @columns.each_with_index do |column, i|
+        if position[1] == column.to_s
+            arr[1] = i + 1
+        end
+    end
+    arr
+  end
+
+  def arr_to_position(arr)
+    position = ''
+    @rows.each do |row|
+        if arr[0] == row.to_i
+            position[0] = row.to_s
+        end
+    end
+    @columns.each_with_index do |column, i|
+        if arr[1] == i + 1
+            position[1] = column.to_s
+        end
+    end
+    position
   end
 
   def print_board
